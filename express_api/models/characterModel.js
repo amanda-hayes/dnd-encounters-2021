172651bb21ded6fffd341d4ec96a70021f3436e0
Amanda Hayes <amandahayes@Amandas-MacBook-Pro.local>
2020-12-28 16:42:25 -0800

    Created react app for front end

--- express_api/models/characterModel.js
+++ express_api/models/characterModel.js
@@ -1,10 +1,14 @@
 const { Schema, model } = require('mongoose');
 
 const characterSchema = Schema({
+    type: { type: String, required: false },
     name: { type: String, required: true },
     race: { type: String, required: true },
     class: { type: String, required: true },
     pronouns: { type: String, required: false },
+    HP: { type: Number, required: false },
+    attack: { type: String, required: false },
+    catchphrases: { type: Object, required: false },
 
 });
 
