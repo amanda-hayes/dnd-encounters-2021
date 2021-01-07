c9a4846c755d8efca6b2e8675deedcb7e8d2bc0e
Amanda Hayes <amandahayes@Amandas-MacBook-Pro.local>
2021-01-07 10:41:39 -0800

    added nav to youlose page

--- front-end/src/Components/YouLose.js
+++ front-end/src/Components/YouLose.js
@@ -1,8 +1,15 @@
 import "../App.css";
+import { Link } from 'react-router-dom';
 
 function YouLose() {
   return (
     <>
+    <nav className="topnav">
+        <Link to="/">HOME</Link>
+        <Link to="/characters">CHARACTERS</Link>
+        <Link to="/createcharacterform">CREATE</Link>
+        <Link to="/battle">BATTLE</Link>
+      </nav>
       <h1>
         Your party has been defeated. Oh, well. It was fun while it lasted!
       </h1>
