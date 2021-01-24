2255426a2928f054fb351af827cc453e49e287a7
Amanda Hayes <amandahayes@Amandas-MacBook-Pro.local>
2021-01-24 15:35:12 -0800

    Added front end component for register functionality

---
+++ front-end/src/Components/Register.js
@@ -0,0 +1,71 @@
+import "../App.css";
+import { Link } from "react-router-dom";
+import { useRef } from 'react';
+
+const RegisterForm = (props) => {
+    const usernameInput = useRef(null);
+    const passwordInput = useRef(null);
+
+    const registerUser = async (event) => {
+        event.preventDefault();
+        const username = usernameInput.current.value;
+        const password = passwordInput.current.value;
+
+        const body = JSON.stringify({
+            username,
+            password
+        });
+
+        event.currentTarget.reset();
+
+        try {
+            const response = await fetch(
+                "http://localhost:7000/User",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-type": "application/json"
+                    },
+                    body: body,
+                }
+            );
+            props.history.push('/Users');
+            alert('Account created!');
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <>
+        <nav className="topnav">
+            <Link to="/">HOME</Link>
+            <Link to="/characters">CHARACTERS</Link>
+            <Link to="/createcharacterform">CREATE</Link>
+            <Link to="/battle">BATTLE</Link>
+            <Link to="/login">LOGIN</Link>
+            <Link to="/register">REGISTER</Link>
+        </nav>
+
+        <div>
+            <h2>Register</h2>
+            <p>
+                Please create a unique username and password to create an account.
+                We suggest passwords be at least 8 characters with letters,
+                numbers, and special characters.
+            </p>
+            <form onSubmit={registerUser} method="post">
+                <label>Username</label>
+                <input type="text" name="username" ref={usernameInput} />
+                <br />
+                <label>Password</label>
+                <input type="text" name="password" ref={passwordInput} />
+                <br />
+                <input type="submit" value="REGISTER" id="submit-btn" />
+            </form>
+        </div>
+        </>
+    );
+};
+
+export default RegisterForm;
