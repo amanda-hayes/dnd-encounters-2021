9ef99a23908e6bcd7b990400486add85c3a4221c
Amanda Hayes <hayesmdesign@gmail.com>
2021-02-04 12:57:23 -0800

    removed package lock json from root

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const app = require("./express_api/app");
-const express = require("express")();
+const express = require("express");
 
 // for react router
+
+const path = require("path");
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(`${__dirname}/build/index.html`));
 });
